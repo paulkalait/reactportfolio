2f692a3a2c60defa36ac435854163ceda3ac4b1d
Your Name <kalaitzidispaul@gmail.com>
2022-07-25 09:39:12 -0400

    touched up bio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,20 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import Aos from "aos";
-import "aos/dist/aos.css"
-
+import "aos/dist/aos.css";
 
 export const About = () => {
   useEffect(() => {
-    Aos.init({ duration: 2000})
-    }, [])
+    Aos.init({ duration: 2000 });
+  }, []);
   return (
-    <div name="about" className="w-full h-[65vh] mt-20 bg-slate-900 text-gray-300 ">
-      <div  data-aos="fade-up" className="flex flex-col justify-center items-center w-full">
+    <div
+      name="about"
+      className="w-full h-[65vh] mt-20 bg-slate-900 text-gray-300 "
+    >
+      <div
+        data-aos="fade-up"
+        className="flex flex-col justify-center items-center w-full"
+      >
         <div className="max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8">
           <div className="sm:text-right pb-8 pl-4">
             <p className="text-4xl font-bold inline border-b-4 border-white">
@@ -26,16 +31,15 @@ export const About = () => {
           </div>
           <div>
             <p>
-              I am currently attending UCONN coding bootcamp to become a full
-              stack developer. Prior I graduated from Central Connecticut state
-              university in a degree in finance and worked in tax; however,
-              coding is where I believe I found my passion. It requires an
-              endless journey of learning and persistence which I value highly.
+              I'm a full stack developer who is passionate in writing clean
+              code and learning new technologies. I Recently graduated from the
+              Univeristy of Connecticut Coding Bootcamp. I also worked in finance and
+              tax; however, coding is where I believe I found my
+              passion. 
             </p>
           </div>
         </div>
       </div>
-      
     </div>
   );
 };
